8e408f23b627eb0d27e4d766fa3cc20fdb2b9a3c
Jonathan Conway <jonathan.conway@gmail.com>
2016-04-18 08:46:56 +1000

    add genre select

---
+++ src/components/GenreSelect.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+export default class GenreSelect extends Component {
+
+  render() {
+    return (
+      <select className="GenreSelect">
+        <option>Genre1</option>
+        <option>Genre2</option>
+        <option>Genre3</option>
+      </select>
+    );
+  }
+
+}
--- src/containers/HomePage.js
+++ src/containers/HomePage.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
-import { FormattedMessage }  from "../utils/IntlComponents";
+import GenreSelect  from "../components/GenreSelect";
 
 export default class HomePage extends Component {
 
   render() {
     return (
       <div>
-        <FormattedMessage message="home.welcome" />
+      	<GenreSelect />
       </div>
     );
   }
